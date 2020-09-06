79629ef5b557507b2a400774f0708215255e4f93
Chad Wescott <cwescott@coopervision.com>
2020-09-06 11:39:17 -0400

    Improved style of round score.

--- src/app/components/round/round.component.ts
+++ src/app/components/round/round.component.ts
@@ -1,10 +1,24 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Round } from 'src/app/models/round';
 
+// import { animate, animation, keyframes, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'ch-round',
   templateUrl: './round.component.html',
-  styleUrls: ['./round.component.scss']
+  styleUrls: ['./round.component.scss'],
+  // animations: [
+  //   trigger('scoreChanged', [
+  //     transition('* => *', [
+  //       animation([
+  //         style({ transform: 'scale(100%)' }),
+  //         animate('0.5s', keyframes([
+  //           style({ transform: 'scale(125%)', offset: 0.50 }),
+  //           style({ transform: 'scale(100%)', offset: 1 })
+  //         ]))
+  //       ])
+  //     ])
+  // ])
+  // ]
 })
 export class RoundComponent implements OnInit {
   @Input()
