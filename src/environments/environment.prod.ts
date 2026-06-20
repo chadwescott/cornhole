dc277ae5e550341cc84df3bfd7b12064f298cb42
Chad Wescott <chad.wescott@ayahealthcare.com>
2026-06-20 11:39:04 -0400

    Added github pages publish script.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    firebaseConfig: {
+        appId: '1:38481895674:web:96fafee8c95121d462e943',
+        apiKey: 'AIzaSyC4jdosu6Jjj_gskDocBNd5qliK_dawx6c',
+        authDomain: 'cornhole-499220.firebaseapp.com',
+        messagingSenderId: '38481895674',
+        projectId: 'cornhole-499220',
+        storageBucket: 'cornhole-499220.firebasestorage.app'
+    },
+    supabase: {
+        url: 'https://vxabasyhedjdjuvwzmvl.supabase.co',
+        publishableKey: 'sb_publishable_QBZctI_mHDMCE2vGoxiCUg_dVCsPcLZ'
+    }
+};
