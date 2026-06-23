c91ba35630ac679e3f8646e9a3fcc1875edbbda1
Chad Wescott <chad.wescott@ayahealthcare.com>
2026-06-23 09:20:57 -0400

    Improved menu and page layouts. Cleaned up and consolidated some CSS rules.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { AppDataService } from './services/app-data.service';
@@ -11,18 +14,12 @@ import { AppDataService } from './services/app-data.service';
   imports: [
     RouterLink,
     RouterLinkActive,
-    RouterOutlet
+    RouterOutlet,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule
   ]
 })
 export class AppComponent {
   readonly appDataService = inject(AppDataService);
-  isMenuOpen = false;
-
-  toggleMenu(): void {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-
-  closeMenu(): void {
-    this.isMenuOpen = false;
-  }
 }
