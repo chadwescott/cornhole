8008be00099abcfbc8e2c175953205a1c64291e5
Chad Wescott <chad.wescott@ayahealthcare.com>
2026-06-24 16:33:26 -0400

    Added a throwing indicator during team games.

--- src/app/components/team-score/team-score.component.ts
+++ src/app/components/team-score/team-score.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { DesignOptions } from '../../models/design-options.enum';
 import { Team } from '../../models/team.model';
 
@@ -8,16 +8,11 @@ import { Team } from '../../models/team.model';
   styleUrls: ['./team-score.component.scss'],
   standalone: true
 })
-export class TeamScoreComponent implements OnInit {
+export class TeamScoreComponent {
   teamNumber = input.required<number>();
   team = input.required<Team>();
   score = input.required<number>();
+  throwIndex = input<number>(0);
 
   designOptions = DesignOptions;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
